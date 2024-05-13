5c69c43a17c83c1e7e1f72cc104689628fbeabf7
mab <malikbello084@gmail.com>
2024-05-13 19:00:49 +0100

    simple google news url scraperr

---
+++ scraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+class Scraper:
+    site = ""
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        response = urlopen(self.site)
+        html = response.read()
+        soup = BeautifulSoup(html, "html.parser")
+
+        for tag in soup.find_all('a'):
+            url = tag.get('href')
+            if url and 'articles' in url and tag.text != "":
+                news_url = "https://news.google.com/" + url[2:]
+                print(tag.text)
+                print(news_url, end="\n\n")
+
+if __name__ == "__main__":
+    Scraper('https://news.google.com/').scrape()
